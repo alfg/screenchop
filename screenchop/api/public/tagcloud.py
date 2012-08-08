6dc55baf04520b8ba977fe73d808c7502bbbc7eb
Alf <alf.g.jr@gmail.com>
2012-08-07 22:22:30 -0700

    Minor tweaks. Merged tagcloud.css over to custom.css.

--- screenchop/api/public/tagcloud.py
+++ screenchop/api/public/tagcloud.py
@@ -3,23 +3,23 @@ tagcloud.py - JSON view to load tag cloud used by /tags view.
 
 '''
 
-from flask import Flask
 from flask import jsonify
-from flask import render_template
 
 from screenchop.models import *
 from screenchop import config
 
 def tagcloud_json():
     ''' Tagcloud json view to generate tagcloud for /tags view. '''
-
+    
+    # Queries Tag_freq collection for all except null/None
     tags = Tag_freq.objects(tag__ne=None)
+    
     #Query list of dictionaries for a JSON object. Construct a json response
     jsonTagsQuery = [
-                        {  "tag": x.tag,
+                        {
+                           "tag": x.tag,
 					       "freq": x.freq
-
-                         } for x in tags
+                        } for x in tags
                     ]
     
     # Return json response
