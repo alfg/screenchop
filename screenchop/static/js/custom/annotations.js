d96d674cf7a8963a4bb2e7be1da6303d116f5e7c
Alf <alf.g.jr@gmail.com>
2012-12-11 23:16:52 -0800

    js page conditional statements

---
+++ screenchop/static/js/custom/annotations.js
@@ -0,0 +1,14 @@
+
+/*
+ * Annotations functions
+ * */
+
+$(window).load(function() {
+				$("#toAnnotate").annotateImage({
+					getUrl: "/api/public/tags.json?filename=" + postfilename,
+					saveUrl: "/tags/save",
+					deleteUrl: "/tags/delete",
+					editable: tagable
+				});
+				$('#image-annotate-add').appendTo('#add-tag-button');
+			});
--- screenchop/static/js/custom/custom.js
+++ screenchop/static/js/custom/custom.js
@@ -215,16 +215,3 @@ $('.regBoxEnter').keypress(function(e){
 });
 
 
-/*
- * Annotations functions
- * */
-
-$(window).load(function() {
-				$("#toAnnotate").annotateImage({
-					getUrl: "/api/public/tags.json?filename=" + postfilename,
-					saveUrl: "/tags/save",
-					deleteUrl: "/tags/delete",
-					editable: tagable
-				});
-				$('#image-annotate-add').appendTo('#add-tag-button');
-			});
